401936c64ab3c720831aa8f29bc96c5e62a7c655
Wesley <wesley@example.com>
2025-05-24 01:08:15 -0400

    chore: added functionality to select pet type

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace VirtualPetSimulator;
+
+class Program
+{
+    static void Main()
+    {
+        // Define available pet types
+        string[] petType = new string[] { "Cat", "Dog", "Bird", "Chinchilla" }
+        ;
+
+        // Set variables for user pet selection
+        int userPetTypeId = 0;
+
+        // Prompt user to choose pet type
+        Console.WriteLine("Please choose a type of pet");
+        for (int i = 0; i < petType.Length; i++)
+        {
+            Console.WriteLine("{0}. {1}", i + 1, petType[i]);
+        }
+
+        // Check that a valid selection was made and set selection
+        string? inputPetType;
+        bool validSelection = false;
+        do
+        {
+            inputPetType = Console.ReadLine();
+            if (int.TryParse(inputPetType, out int selected))
+            {
+                if (selected > 0 && selected <= petType.Length)
+                {
+                    userPetTypeId = selected - 1;
+                    validSelection = true;
+                    Console.WriteLine("\nUser Input: {0}", selected);
+                    break;
+                }
+            }
+            Console.WriteLine("\nPlease choose a number between 1 and " + petType.Length);
+        } while (!validSelection);
+        
+        Console.WriteLine("\nYou have chosen a {0}. What would like to name your pet?", petType[userPetTypeId]);
+    }
+}
